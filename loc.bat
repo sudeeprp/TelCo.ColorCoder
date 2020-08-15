f7cfb9856431ea441834514fb3e3373c4e70fcd0
mb <madhura.bindu@gmail.com>
2020-08-15 13:43:37 +0530

    Update loc.bat

--- loc.bat
+++ loc.bat
@@ -3,6 +3,6 @@ set file=TelCo.ColorCoder\program.cs
 set /a cnt=0
 for /f %%a in ('type "%file%"^|find "" /v /c') do set /a cnt=%%a
 echo %file% has %cnt% lines
-if (cnt GTR 100) exit 0
-echo Modularity check failed. Max allowed lines 10
+if (cnt LEQ 100) exit 0
+echo Modularity check failed. Max allowed lines 100
 exit 300
